03f675706de0676aadffa808cd6d1aabd2b735b5
JoseLeonTM <jleon@tekmexico.com>
2017-05-18 09:56:03 -0700

    Finished exercise 170

---
+++ 170/170.js
@@ -0,0 +1,18 @@
+var arr1 = [2, 3, 5, 3], arr2 = [3, 4, 3, 5, 3, 6, 8]; //[3,5,3]
+// var arr1 : number[] = [2,2,3,5,3,7,8],
+//     arr2 : number[] = [3,2,1,4,3,5,3,6,8];//[2,3,5,3,8]
+function arrayIntersection(arr1, arr2) {
+    let newArr = [];
+    let cur = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        for (let o = cur; o < arr2.length; o++) {
+            if (arr2[o] == arr1[i]) {
+                newArr.push(arr2[o]);
+                cur = o + 1;
+                break;
+            }
+        }
+    }
+    return newArr;
+}
+console.log(arrayIntersection(arr1, arr2));
