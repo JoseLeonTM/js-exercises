c9aee2e14c3ddba1756afd87f2ee3e5fc6386960
JoseLeonTM <jleon@tekmexico.com>
2017-05-22 10:00:45 -0700

    Finished exercise 179

---
+++ 179/179.js
@@ -0,0 +1,37 @@
+var obj1 = {
+    name: 'Carlos',
+    last: 'Salazar',
+    age: '23',
+    job: 'programmer'
+}, obj2 = {
+    animal: 'Lion',
+    name: 'Alex',
+    age: '3',
+    color: 'orange'
+}, obj3 = {
+    brand: 'Toyota',
+    model: '2012',
+    name: 'Corolla',
+    color: 'red'
+};
+function checkProps(o1, o2) {
+    let props = {};
+    for (let prop in o1) {
+        if (o2[prop]) {
+            if (o1[prop] !== o2[prop]) {
+                props[prop] = [o1[prop], o2[prop]];
+            }
+        }
+        else {
+            props[prop] = o1[prop];
+        }
+    }
+    for (let prop in o2) {
+        if (!props[prop]) {
+            props[prop] = o2[prop];
+        }
+    }
+    return props;
+}
+console.log(checkProps(obj1, obj2));
+console.log(checkProps(obj1, obj3));
