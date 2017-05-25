000b2cac1009047c6c174adcf73e42e1ca375acc
JoseLeonTM <jleon@tekmexico.com>
2017-05-25 16:00:51 -0700

    Finished exercise 191

---
+++ 191/191.js
@@ -0,0 +1,14 @@
+var btn = document.getElementsByTagName('button')[0];
+var container = document.getElementById('container');
+var changeColor = (function () {
+    var colors = ['blue', 'red', 'yellow', 'green', 'gray', 'lightblue', 'chocolate', 'darkgoldenrod', 'wheat', 'brown'];
+    var divs = container.getElementsByTagName('div');
+    return function () {
+        let col = Math.floor(Math.random() * 10);
+        let div = Math.floor(Math.random() * 10);
+        // console.log("color: ",col);
+        // console.log("div: ",div);
+        divs[div].style.backgroundColor = colors[col];
+    };
+})();
+btn.addEventListener('click', changeColor);
