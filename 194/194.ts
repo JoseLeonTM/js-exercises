a7ef38084f41dd2ef96836fea12196a8d33fbf69
JoseLeonTM <jleon@tekmexico.com>
2017-05-26 15:16:06 -0700

    Finished exercise 194

---
+++ 194/194.ts
@@ -0,0 +1,16 @@
+
+Array.prototype.remove = function(...elements:Array<any>):Array<any>{
+    let newArray :Array<any>= [];
+    newArray.push(...this);
+    elements.sort((a,b)=>b-a);
+    for(let i=0; i<elements.length;i++){
+        if(elements[i]>=0 && elements[i]<newArray.length){
+            newArray.splice(elements[i],1);
+        }
+    }
+    return newArray;
+};
+var arr=[1,2,3,4,5,6,7];
+var arr2 = arr.remove(1,2,6);
+console.log("Original array: ",arr);
+console.log("Trimmed array: ",arr2);
