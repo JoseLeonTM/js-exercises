d41e34a9cf4f01b27eb77061f807d4b67cae963e
JoseLeonTM <jleon@tekmexico.com>
2017-05-26 17:39:58 -0700

    Working on exercise 195

---
+++ 195/195.ts
@@ -0,0 +1,17 @@
+
+var left = document.getElementById('left');
+var right= document.getElementById('right');
+var loginBtn = right.getElementsByTagName('li')[1];
+var login = right.getElementsByClassName('login')[0];
+var goBtn = document.getElementById('go');
+
+document.addEventListener('click',function(ev){
+    console.log(ev.target);
+    if(ev.target!=login){
+        login.className='login';
+    }
+});
+loginBtn.addEventListener('click',function(){
+    login.className='login';
+    console.log(login);
+});
