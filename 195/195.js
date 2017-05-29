1e3f366e98db27ee72f6ac7563a1d8a6f42c5750
JoseLeonTM <jleon@tekmexico.com>
2017-05-29 12:28:44 -0700

    Finished exercise 195

--- 195/195.js
+++ 195/195.js
@@ -4,12 +4,17 @@ var loginBtn = right.getElementsByTagName('li')[1];
 var login = right.getElementsByClassName('login')[0];
 var goBtn = document.getElementById('go');
 document.addEventListener('click', function (ev) {
-    console.log(ev.target);
-    if (ev.target != login) {
-        login.className = 'login';
+    var isLogin = ev.target;
+    if (isLogin != loginBtn) {
+        while (isLogin) {
+            if (isLogin == login) {
+                return;
+            }
+            isLogin = isLogin.parentNode;
+        }
+        login.className = 'login hide';
     }
 });
 loginBtn.addEventListener('click', function () {
     login.className = 'login';
-    console.log(login);
 });
