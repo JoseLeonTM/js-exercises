cd3aa6f722aa8fff20959720c9a137fe8aa74fc3
JoseLeonTM <jleon@tekmexico.com>
2017-05-30 12:34:43 -0700

    Finished exercise 200

---
+++ 200/200.ts
@@ -0,0 +1,11 @@
+
+
+var div = document.getElementsByTagName('div')[0];
+var a = document.getElementById('id1');
+
+div.className='hide';
+
+a.addEventListener('click',function(){
+    div.className='';
+});
+
