178ffb7f0908dcf28fa0e952ba2fcced1582a582
JoseLeonTM <jleon@tekmexico.com>
2017-05-30 12:54:23 -0700

    Finished exercise 201

---
+++ 201/201.ts
@@ -0,0 +1,15 @@
+
+var Ps = document.getElementsByTagName('p');
+for(let i =1; i< Ps.length; i=i+2){
+    Ps[i].className='hide';
+}
+for(let i =0; i<Ps.length; i=i+2){
+    Ps[i].addEventListener('click',function(){
+        if(!Ps[i+1].className){
+            Ps[i+1].className='hide';
+        }
+        else{
+            Ps[i+1].className='';
+        }
+    });
+}
